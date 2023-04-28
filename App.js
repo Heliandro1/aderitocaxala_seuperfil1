92bc72f406d2b184457914b347b034d4edc761cd
ACC <64018848+Heliandro1@users.noreply.github.com>
2023-04-28 01:51:51 +0100

    creating the profile page

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-import 'expo-router/entry';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Profile from './src/pages/your-profile';
+
+export default function App() {
+  return (
+    <Profile />
+  );
+}
+
